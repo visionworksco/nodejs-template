e04a2fb89d396fe57ce1f5b160904b0cd47463ff
spetushkov <sergey.petushkov@gmail.com>
2022-04-16 22:26:21 +0200

    feat: :rocket:

--- src/app/config/Config.ts
+++ src/app/config/Config.ts
@@ -13,7 +13,18 @@ Config
   .file({ file: path.join(__dirname, 'config.json') })
   // 4. Defaults
   .defaults({
-    App: {},
+    App: {
+      API_VERSION: 'v1',
+      ACCESS_CONTROL_ALLOW_ORIGIN: '*',
+      ACCESS_CONTROL_ALLOW_HEADERS:
+        'Origin,X-Requested-With,Content-Type,Accept,Authorization,Cookie',
+      ACCESS_CONTROL_ALLOW_METHODS: 'HEAD, GET, POST, PATCH, DELETE, OPTIONS',
+      HTTP_TIMEOUT: 10000,
+      API_DOC_PATH: '/api-doc',
+      JWT_SECRET: 'JWT_SECRET',
+      FILE_UPLOAD_PATH: '/',
+      CLIENT_BUILD_PATH: '/',
+    },
   })
   // 5. Required varaibles
   .required([]);
